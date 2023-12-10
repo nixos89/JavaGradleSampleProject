6b27d16d884861adf2d0692e97d8ee981813a84a
Nikola Stevanović <nixos89@gmail.com>
2023-12-10 22:54:06 +0100

    Initial commit

---
+++ src/main/java/com/nikolas/JavaGradleSampleProject/JavaGradleSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nikolas.JavaGradleSampleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGradleSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGradleSampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikolas/JavaGradleSampleProject/JavaGradleSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolas.JavaGradleSampleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGradleSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
